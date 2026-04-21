c8d84ed80cdc2b6c8b3f3fbc75ce504617db0115
kanamugura <137117887+kanamugura@users.noreply.github.com>
2026-04-21 19:53:48 +0300

    Add files via upload

---
+++ sample_test.py
@@ -0,0 +1,4 @@
+def test_main_page_title():
+    expected = "Комитет по энергетике и инженерному обеспечению"
+    actual = "Комитет по энергетике и инженерному обеспечению"
+    assert actual == expected
